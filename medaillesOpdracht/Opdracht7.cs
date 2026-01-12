5bfcde8fa440640b68e77a51096cb9d7a89dc9e7
mikoreo <michael@de-ru.net>
2026-01-12 11:40:48 +0100

    opdracht 7 bronze

---
+++ medaillesOpdracht/Opdracht7.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medaillesOpdracht
+{
+    internal class Opdracht7
+    {
+        public void Start()
+        {
+            Random dobbelsteen = new Random();
+            int dobbelsteenCijfer = dobbelsteen.Next(1, 7);
+            Console.WriteLine(dobbelsteenCijfer);
+        }
+    }
+}
